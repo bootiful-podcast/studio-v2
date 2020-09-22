015cfb9bff49d7079d9204b5dee7fd3781f9857f
Josh Long <josh@joshlong.com>
2020-09-22 03:15:30 -0700

    up

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
