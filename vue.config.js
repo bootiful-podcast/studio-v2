de212979c7efb2aa7e684620fe06aa34d3ee975e
Josh Long <josh@joshlong.com>
2020-10-29 20:00:04 -0700

    config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,18 @@ process.env.VUE_APP_VERSION = require ('/package.json').version
 
 
 module.exports = {
- name: "A Bootiful Podcast Studio"
+
+
+ index: {
+  // the source template
+  template: 'public/index.html',
+
+  // output as dist/index.html
+  // when using title option,
+  // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+  title: 'A Bootiful Podcast Studio',
+  // chunks to include on this page, by default includes
+  // extracted common chunks and vendor chunks.
+  chunks: ['chunk-vendors', 'chunk-common', 'index']
+ },
 }
