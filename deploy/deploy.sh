1ec0e58400a506cbca01235a09daf7979aa6a32f
Josh Long <josh@joshlong.com>
2020-12-02 00:43:52 -0800

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,26 +4,30 @@ set -e
 set -o pipefail
 
 export BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE:-development}
-export ENV_SUB_DOMAIN=$( [ $BP_MODE_LOWERCASE = "production" ] && echo ""  || echo "${BP_MODE_LOWERCASE}.")
-export APP_NAME=studio
-export PROJECT_ID=${GCLOUD_PROJECT}
+export ENV_SUB_DOMAIN=$( [ "${BP_MODE_LOWERCASE}" = "production" ] && echo ""  || echo "${BP_MODE_LOWERCASE}.")
 export ROOT_DIR=$(cd $(dirname $0) && pwd)
 export OD=${ROOT_DIR}/overlays/${BP_MODE_LOWERCASE}
+export APP_NAME=studio
+export PROJECT_ID=${GCLOUD_PROJECT}
 export GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 
-echo "The ENV_SUB_DOMAIN=$ENV_SUB_DOMAIN"
+echo "ENV_SUB_DOMAIN=${ENV_SUB_DOMAIN}"
+echo "BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE}"
 
+
+cd $(dirname $0)/..
+ROOT_DIR=$(pwd)
 cd $ROOT_DIR
 
 rm -rf $ROOT_DIR/build
 rm -rf $ROOT_DIR/dist
 
 PROD_ENV_FILE=${ROOT_DIR}/.env.production
-rm -rf  $PROD_ENV_FILE || echo "no file to delete.."
+rm $PROD_ENV_FILE
 touch $PROD_ENV_FILE
 echo "VUE_APP_SERVICE_ROOT=https://api.${ENV_SUB_DOMAIN}bootifulpodcast.fm" >> ${PROD_ENV_FILE}
-echo "VUE_APP_GIT_HASH=${GITHUB_SHA}" >>  ${PROD_ENV_FILE}
-echo "VUE_APP_BP_MODE=${BP_MODE_LOWERCASE}" >>  ${PROD_ENV_FILE}
+echo "VUE_APP_GIT_HASH=${GITHUB_SHA}" >> ${PROD_ENV_FILE}
+echo "VUE_APP_BP_MODE=${BP_MODE_LOWERCASE}" >> ${PROD_ENV_FILE}
 
 echo "--------------------------"
 echo "The production environment file contains: "
@@ -44,8 +48,10 @@ echo "pushing ${image_id} to gcr.io/${PROJECT_ID}/${APP_NAME}"
 echo "tagging ${GCR_IMAGE_NAME}"
 docker tag "${image_id}" ${GCR_IMAGE_NAME}
 echo "finished tag"
-
 docker push ${GCR_IMAGE_NAME}
 echo "finished push"
 
+export RESERVED_IP_NAME=${APP_NAME}-${BP_MODE_LOWERCASE}-ip
+gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME ||
+  gcloud compute addresses create $RESERVED_IP_NAME --global
 kubectl apply -k ${OD}
