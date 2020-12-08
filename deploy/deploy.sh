3a4a9c64128f86def7bb8bda79247ff9ea285590
Josh Long <josh@joshlong.com>
2020-12-08 14:35:25 -0800

    up

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,17 +3,13 @@
 set -e
 set -o pipefail
 
+export APP_NAME=studio
 export BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE:-development}
 export ENV_SUB_DOMAIN=$( [ "${BP_MODE_LOWERCASE}" = "production" ] && echo ""  || echo "${BP_MODE_LOWERCASE}.")
 export ROOT_DIR=$(cd $(dirname $0) && pwd)
 export OD=${ROOT_DIR}/overlays/${BP_MODE_LOWERCASE}
-export APP_NAME=studio
 export PROJECT_ID=${GCLOUD_PROJECT}
 export GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
-
-echo "ENV_SUB_DOMAIN=${ENV_SUB_DOMAIN}"
-echo "BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE}"
-
 
 cd $(dirname $0)/..
 ROOT_DIR=$(pwd)
@@ -25,7 +21,8 @@ rm -rf $ROOT_DIR/dist
 PROD_ENV_FILE=${ROOT_DIR}/.env.production
 rm $PROD_ENV_FILE
 touch $PROD_ENV_FILE
-echo "VUE_APP_SERVICE_ROOT=https://api.${ENV_SUB_DOMAIN}bootifulpodcast.fm" >> ${PROD_ENV_FILE}
+echo "VUE_APP_API_ROOT=${API_ROOT}"
+echo "VUE_APP_SERVICE_ROOT=${API_ROOT}" >> ${PROD_ENV_FILE}
 echo "VUE_APP_GIT_HASH=${GITHUB_SHA}" >> ${PROD_ENV_FILE}
 echo "VUE_APP_BP_MODE=${BP_MODE_LOWERCASE}" >> ${PROD_ENV_FILE}
 
@@ -44,14 +41,13 @@ cd $ROOT_DIR/build
 pack build $APP_NAME --builder paketobuildpacks/builder:full --buildpack gcr.io/paketo-buildpacks/nginx:latest  --env PORT=8080
 image_id=$(docker images -q $APP_NAME)
 
+docker tag "${image_id}" ${GCR_IMAGE_NAME}
+docker push ${GCR_IMAGE_NAME}
 echo "pushing ${image_id} to gcr.io/${PROJECT_ID}/${APP_NAME}"
 echo "tagging ${GCR_IMAGE_NAME}"
-docker tag "${image_id}" ${GCR_IMAGE_NAME}
-echo "finished tag"
-docker push ${GCR_IMAGE_NAME}
-echo "finished push"
 
 export RESERVED_IP_NAME=${APP_NAME}-${BP_MODE_LOWERCASE}-ip
 gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME ||
   gcloud compute addresses create $RESERVED_IP_NAME --global
 kubectl apply -k ${OD}
+kubectl patch deployment $APP_NAME -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": { \"redeploy\": \"$(date +%s)\"}}}}}"
